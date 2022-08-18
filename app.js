b94c2ec5a8ed215b4d2e203b0e5db3fb9a80129b
AminAbdal <107882502+aminabdal13@users.noreply.github.com>
2022-08-18 08:22:34 -0700

    Update app.js
    fix the (uncaught (in promise) syntaxerror: unexpected token '<', "<!doctype "... is not valid json) bog in line 21;
    add Try Catch to controll undifind Citys

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const humidity = document.querySelector(".Humidity");
 const WindSpeed = document.querySelector(".WindSpeed");
 const variable = document.querySelector(".var");
 
+
 const APIkey = "f123c758b483a32efc7183059acb012c";
 
 
@@ -15,23 +16,29 @@ ShowBtn.addEventListener("click",HandeledShowCity);
 
 function HandeledShowCity(e){
     console.log(input.value)
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${APIkey}&units=metric`
-    fetch(url)
-        .then(response => response.json())
-        .then(data =>{
-            console.log(data)
-            const {main,name,weather,wind} = data
-            city.innerText = name;
-            city.style.display = "block";
-            temp.innerHTML=Math.round(+main.temp)+`C <sup style="color: #fff">o</sup>`;
-            temp.style.display = "block";
-            status.innerText = weather[0].main;
-            status.style.display = "block";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${APIkey}&units=metric`;
 
-            deg.innerHTML = Math.round(main.feels_like)+`C <sup style="color: #000">o</sup>`;
-            humidity.innerText = main.humidity;
-            WindSpeed.innerHTML = Math.round(+wind.speed)+` Km/h`;
-
-            variable.style.display = "block";
-        })
+        fetch(url)
+            .then(Response => Response.json())
+            .then(data =>{
+                try{
+                    console.log(data)
+                    const {main,name,weather,wind,sys} = data
+                    console.log(sys);
+                    city.innerHTML = name + `<sup class='country' >${sys.country}</sup>`;
+                    city.style.display = "block";
+                    temp.innerHTML=Math.round(+main.temp)+`C <sup style="color: #fff">o</sup>`;
+                    temp.style.display = "block";
+                    status.innerText = weather[0].main;
+                    status.style.display = "block";
+        
+                    deg.innerHTML = Math.round(main.feels_like)+`C <sup style="color: #000">o</sup>`;
+                    humidity.innerText = main.humidity;
+                    WindSpeed.innerHTML = Math.round(+wind.speed)+` Km/h`;
+        
+                    variable.style.display = "block";
+                }catch (err){
+                    city.innerText = 'city not found';
+                }
+            })        
 }
